c1dbbe443ce22df788250922e945c9530359729b
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-01-13 20:09:52 -0600

    Added bio, goals, work and school history. Updated CSS.

--- js/app.js
+++ js/app.js
@@ -7,27 +7,44 @@ alert('Hello, ' + Username + ' Nice to Meet you!');
 
 
 var questionOne = prompt('Am I Married?').toLowerCase();
-if(questionOne === 'yes' || questionOne === 'y') {
+if (questionOne === 'yes' || questionOne === 'y') {
   // console.log('Your correct');
   alert('You Are Correct');
 }
 
 var questionTwo = prompt('Do I like Beer?').toLowerCase();
-if(questionTwo === 'yes' || questionTwo === 'y') {
+if (questionTwo === 'yes' || questionTwo === 'y') {
   // console.log('Your Correct!);
   alert('You Are Correct');
-} else if ( questionTwo === 'no' || questionTwo === 'n' ) {
+} else if (questionTwo === 'no' || questionTwo === 'n') {
+  // console.log('EHHHH, WRONG!');
   alert('EHHHH, WRONG!');
 }
 
 var questionThree = prompt('Will I die young?').toLowerCase();
-if(questionThree === 'yes' || questionThree === 'y' ) {
+if (questionThree === 'yes' || questionThree === 'y') {
   // console.log('Your Correct!);
   alert('You are Correct');
-} else if ( questionThree === 'no' || questionThree === 'y' ) {
+} else if (questionThree === 'no' || questionThree === 'n') {
+  // console.log('Nope Good Try');
   alert('Nope Good Try');
 }
 
+var questionFour = prompt('Do I have 2 boys?').toLowerCase();
+if (questionFour === 'yes' || questionFour === 'y') {
+  // console.log('Your correct!');
+} else if (questionFour === 'no' || questionFour === 'n') {
+  //console.log('Incorrect');
+  alert('Incorrect');
+}
 
+var questionFive = prompt('Would I like Snickers?').toLowerCase();
+if (questionFive === 'yes' || questionFive === 'y') {
+  // console.log('Your Correct');
+  alert('Your Correct');
+} else if (questionFive === 'no' || questionFive === 'n') {
+  // console.log('That is not correct');
+  alert('That is not correct');
+}
 
 
