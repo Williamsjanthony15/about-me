e98dabefd44a1e0022627f643910af4edaadee18
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-01-13 22:23:14 -0600

    did some CSS JS and HTML. Pushing for Submition

--- js/app.js
+++ js/app.js
@@ -1,48 +1,48 @@
-'use strict';
-//Get Username
-// write 5 questions, Must accepct y/n or Yes/No in ANY CASE
-// User Username in the beginning and End when the user is done.
-var Username = prompt('Hey! Whats your name?');
-alert('Hello, ' + Username + ' Nice to Meet you!');
+// 'use strict';
+// //Get Username
+// // write 5 questions, Must accepct y/n or Yes/No in ANY CASE
+// // User Username in the beginning and End when the user is done.
+// var Username = prompt('Hey! Whats your name?');
+// alert('Hello, ' + Username + ' Nice to Meet you!');
 
 
-var answerOne = prompt('Am I Married?').toLowerCase();
-if (answerOne === 'yes' || answerOne === 'y') {
-  // console.log('Your correct');
-  alert('You Are Correct');
-}
+// var answerOne = prompt('Am I Married?').toLowerCase();
+// if (answerOne === 'yes' || answerOne === 'y') {
+//   // console.log('Your correct');
+//   alert('You Are Correct');
+// }
 
-var answerTwo = prompt('Do I like Beer?').toLowerCase();
-if (answerTwo === 'yes' || answerTwo === 'y') {
-  // console.log('Your Correct!);
-  alert('You Are Correct');
-} else if (answerTwo === 'no' || answerTwo === 'n') {
-  // console.log('EHHHH, WRONG!');
-  alert('EHHHH, WRONG!');
-}
+// var answerTwo = prompt('Do I like Beer?').toLowerCase();
+// if (answerTwo === 'yes' || answerTwo === 'y') {
+//   // console.log('Your Correct!);
+//   alert('You Are Correct');
+// } else if (answerTwo === 'no' || answerTwo === 'n') {
+//   // console.log('EHHHH, WRONG!');
+//   alert('EHHHH, WRONG!');
+// }
 
-var answerThree = prompt('Will I die young?').toLowerCase();
-if (answerThree === 'yes' || answerThree === 'y') {
-  // console.log('You only wish');
-  alert('You only wish');
-} else if (answerThree === 'no' || answerThree === 'n') {
-  // console.log('Duh');
-  alert('Duh');
-}
+// var answerThree = prompt('Will I die young?').toLowerCase();
+// if (answerThree === 'yes' || answerThree === 'y') {
+//   // console.log('You only wish');
+//   alert('You only wish');
+// } else if (answerThree === 'no' || answerThree === 'n') {
+//   // console.log('Duh');
+//   alert('Duh');
+// }
 
-var answerFour = prompt('Do I have 2 boys?').toLowerCase();
-if (answerFour === 'yes' || answerFour === 'y') {
-  // console.log('Your correct!');
-} else if (answerFour === 'no' || answerFour === 'n') {
-  //console.log('Incorrect');
-  alert('Incorrect');
-}
+// var answerFour = prompt('Do I have 2 boys?').toLowerCase();
+// if (answerFour === 'yes' || answerFour === 'y') {
+//   // console.log('Your correct!');
+// } else if (answerFour === 'no' || answerFour === 'n') {
+//   //console.log('Incorrect');
+//   alert('Incorrect');
+// }
 
-var answerFive = prompt('Would I like Snickers?').toLowerCase();
-if (answerFive === 'yes' || answerFive === 'y') {
-  // console.log('Your Correct');
-  alert('Your Correct');
-} else if (answerFive === 'no' || answerFive === 'n') {
-  // console.log('That is not correct');
-  alert('That is not correct');
-}
+// var answerFive = prompt('Would I like Snickers?').toLowerCase();
+// if (answerFive === 'yes' || answerFive === 'y') {
+//   // console.log('Your Correct');
+//   alert('Your Correct');
+// } else if (answerFive === 'no' || answerFive === 'n') {
+//   // console.log('That is not correct');
+//   alert('That is not correct');
+// }
