4ba1f1205b4cd7373b81d139d72b581bf476af6d
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-01-24 14:58:47 -0600

    Deleted COmments and checked over for 100% completeness. Ready for turn in

--- js/app.js
+++ js/app.js
@@ -1,82 +1,68 @@
 'use strict';
-//Get Username
-// write 5 questions, Must accepct y/n or Yes/No in ANY CASE
-// User Username in the beginning and End when the user is done.
+
 var score = 0;
 
 var userName = prompt('Hey! Whats your name?');
 alert('Hello, ' + userName + ' Nice to Meet you!');
 
-// Question One
+
 function timeOne() {
   var answerOne = prompt('Am I Married?').toLowerCase();
   if (answerOne === 'yes' || answerOne === 'y') {
-    // console.log('Your correct');
     alert('You Are Correct');
     score++;
   } if (answerOne === 'no' || answerOne === 'n') {
-    // console.log('That isnt correct, Im sorry');
     alert('That isnt correct, Im sorry');
   }
 }
 
 timeOne();
 
-// Question Two
+
 
 function timeTwo() {
   var answerTwo = prompt('Do I like Beer?').toLowerCase();
   if (answerTwo === 'yes' || answerTwo === 'y') {
-    // console.log('Your Correct!);
     alert('You Are Correct');
     score++;
   } else if (answerTwo === 'no' || answerTwo === 'n') {
-    // console.log('EHHHH, WRONG!');
     alert('EHHHH, WRONG!');
   }
 }
 
 timeTwo();
 
-// Question Three
 function timeThree() {
   var answerThree = prompt('Will I die young?').toLowerCase();
   if (answerThree === 'yes' || answerThree === 'y') {
-    // console.log('You only wish');
     alert('You only wish');
     score++;
   } else if (answerThree === 'no' || answerThree === 'n') {
-    // console.log('Duh');
     alert('Duh');
   }
 }
 
 timeThree();
 
-// Question four
+
 function timeFour() {
   var answerFour = prompt('Do I have 2 boys?').toLowerCase();
   if (answerFour === 'yes' || answerFour === 'y') {
-    // console.log('Your correct!');
-    alert('correct!');
+    alert('Correct!');
     score++;
   } else if (answerFour === 'no' || answerFour === 'n') {
-    //console.log('Incorrect');
     alert('Incorrect');
   }
 }
 
 timeFour();
 
-// Question Five
 function timeFive() {
   var answerFive = prompt('Would I like Snickers?').toLowerCase();
   if (answerFive === 'yes' || answerFive === 'y') {
-    // console.log('Your Correct');
-    alert('Your Correct! ' + userName + ' I appreciate the time youve spent with me');
+    alert('You\'re Correct! ' + userName + ' I appreciate the time you\'ve spent with me');
     score++;
   } else if (answerFive === 'no' || answerFive === 'n') {
-    // console.log('That is not correct');
     alert('That is not correct, Thanks for Trying anyway ' + userName + ', I appreciate the time youve spent with me!' + ' You Answered' + score + ' Great Job!');
   }
 }
@@ -111,29 +97,22 @@ function timeSix() {
 timeSix();
 
 function timeSeven() {
-  // I is utilized for loop above.
   var answerCorrectly = false;
   var correctAnswers = ['blue', 'green', 'yellow'];
   for (var i = 0; i < 6; i++) {
     var answerSeven = prompt('What is my favorite Color').toLowerCase();
-    // if (answerSeven === correctAnswers) {
-    //   alert('I\'m not quite sure how you knew that, but good job');
-    // } else {
     for (var j = 0; j < correctAnswers.length; j++) {
       if (correctAnswers[j] === answerSeven) {
         alert('Correct! Good job.');
         answerCorrectly = true;
       }
-      // if (j === correctAnswers.length - 1){
-      //   alert('Input Proper Variable Please.');
-      // }
-
     }
     if (answerCorrectly) {
       break;
-    } 
+    }
   }
 }
 timeSeven();
-{ alert('congratulations!, you answered ' + score + ' correctly! Thanks for visiting. Here are the correct answers: 5, Blue, Green, Yellow. Thanks for playing!');
+{
+  alert('congratulations!, you answered ' + score + ' correctly! Thanks for visiting. Here are the correct answers: 5, and Blue, Green, or Yellow. Thanks for playing!');
 }
